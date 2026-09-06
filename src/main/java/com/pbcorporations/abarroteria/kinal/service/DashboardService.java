9ac4479da1baeb5d4d92acce3b2a825b2077f0f5
Limonx67Bits <lemmon2octv@gmail.com>
2026-09-06 00:12:59 -0600

    Se arreglaron errores en el codigo

--- src/main/java/com/pbcorporations/abarroteria/kinal/service/DashboardService.java
+++ src/main/java/com/pbcorporations/abarroteria/kinal/service/DashboardService.java
@@ -1,5 +1,6 @@
 package main.java.com.pbcorporations.abarroteria.kinal.service;
 
+import java.math.BigDecimal;
 import javafx.collections.ObservableList;
 import main.java.com.pbcorporations.abarroteria.kinal.model.Producto;
 import main.java.com.pbcorporations.abarroteria.kinal.repository.ProductoRepository;
@@ -28,37 +29,23 @@ public class DashboardService {
     public boolean agregarProducto(Producto producto) {
         if (producto == null) {
             throw new RuntimeException("Los datos del producto están vacíos");
-        }else if (producto.getNombreProducto() == null || producto.getNombreProducto().isEmpty()) {
-            throw new RuntimeException("El nombre del producto no puede estar en blanco");
+        }else if (producto.getNombreProducto() == null || producto.getNombreProducto().isEmpty()
+                || producto.getPrecio() == null || producto.getPrecio().compareTo(BigDecimal.ZERO) <= 0
+                || producto.getStock() <= 0) {
+            throw new RuntimeException("Revise los datos ingresados");
         }
        return repository.agregar(producto);     
         
     }
-}
-package main.java.com.pbcorporations.abarroteria.kinal.service;
-
-import javafx.collections.ObservableList;
-import main.java.com.pbcorporations.abarroteria.kinal.model.Producto;
-import main.java.com.pbcorporations.abarroteria.kinal.repository.ProductoRepository;
-
-public class DashboardService {
-    private final ProductoRepository repository;
     
-    public DashboardService(ProductoRepository repository){
-        this.repository = repository;
-    }
-    
-    public ObservableList<Producto> getListaProductos(){
-        if(repository.findAll() == null){
-            throw new RuntimeException("Sin productos");
+    public boolean actualizarProducto(Producto producto) {
+        if(producto == null){
+            throw new RuntimeException("Los datos del producto son vacios");
+        }else if (producto.getNombreProducto() == null || producto.getNombreProducto().isEmpty()
+                || producto.getPrecio() == null || producto.getPrecio().compareTo(BigDecimal.ZERO) <= 0
+                || producto.getStock() <= 0){
+            throw new RuntimeException("Revise los datos ingresados");
         }
-        return repository.findAll();
-    }
-    
-    public boolean eliminarProducto(Producto producto){
-        if(producto != null){
-            return repository.delete(producto);
-        }
-        return false;
+        return repository.update(producto);
     }
 }
